b7feca7205a8502169052cc2ea948d2fcc018979
Kate Kiatsiri <keidsiri@gmail.com>
2021-10-22 10:54:16 -0700

    Adding User interface and Business logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 //business logic
+function addResult(a,b,c,d) {
+  return a + b + c + d ;
+}
+
+
+
+
+
+// user interface
+$(document).ready(function() {
+  $(".userInput").submit(function(event) {
+    event.preventDefault();
+    $(".result").show();
+    const genderInp = parseInt($("#gender").val());
+    // console.log(genderInt);
+    const musicInp = parseInt($("#typemusic").val());
+    const colorInp = parseInt($("#color").val());
+    const animalInt = parseInt($("#animal").val());
+    const totalResult = addResult(genderInp, musicInp, colorInp, animalInt);
+    $(".reult").text(codingLanguage(totalResult);)
+  });
+});
 
 
 
@@ -7,18 +29,9 @@
 
 
 
-
-
-
-
-
-
-// user logic
-
-
-
-
-
-
+// result 0-4 : Swift
+// result 5-7 : Javascript
+// result 8-10 : Python 
+// result 11-13 : Ruby
 
 
