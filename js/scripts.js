bcad73680b37070c2051c219dff3a380fa8696b9
Kate Kiatsiri <keidsiri@gmail.com>
2021-10-22 14:05:58 -0700

    Making some changes on html and css

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,26 @@ function addResult(a,b,c,d) {
 function codingLanguage(result) {
   if (result > 3 && result <= 6 ) {
     $(".swift").show();
+    $(".javascript").hide();
+    $(".python").hide();
+    $(".ruby").hide();
   } else if (result > 6 && result <= 9 ) {
-    return "Your programming language suggestion is : Javascript";
+    $(".swift").hide();
+    $(".javascript").show();
+    $(".python").hide();
+    $(".ruby").hide();
   } else if (result > 9 && result <= 12 ) {
-    return "Your programming language suggestion is : Python";
+    $(".swift").hide();
+    $(".javascript").hide();
+    $(".python").show();
+    $(".ruby").hide();
   } else if (result > 12 && result <= 15 ) {
-    return "Your programming language suggestion is : Ruby";
+    $(".swift").hide();
+    $(".javascript").hide();
+    $(".python").hide();
+    $(".ruby").show();
   } else {
-    alert("Please fill out all the question!");
+    alert("Please fill out the question!");
     // return "Please fill out all the question";
   }
 }
@@ -24,13 +36,13 @@ function codingLanguage(result) {
 $(document).ready(function() {
   $(".userInput").submit(function(event) {
     event.preventDefault();
-    $(".result").show();
     const edInp = parseInt($("#education").val());
     const musicInp = parseInt($("#typeMusic").val());
     const colorInp = parseInt($("#color").val());
     const animalInt = parseInt($("#animal").val());
     const totalResult = addResult(edInp, musicInp, colorInp, animalInt);
     $(".result").text(codingLanguage(totalResult));
+    $(".result").show();
   });
 });
 
