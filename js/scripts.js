2af5a408e360bb1278532fef1657ff47fe3ea4cb
Kate Kiatsiri <keidsiri@gmail.com>
2021-10-22 13:20:17 -0700

    Making some changes scripts.js and add more bootstrap on html, adding more styles on css

--- js/scripts.js
+++ js/scripts.js
@@ -3,35 +3,37 @@ function addResult(a,b,c,d) {
   return a + b + c + d ;
 }
 
+function codingLanguage(result) {
+  if (result > 3 && result <= 6 ) {
+    $(".swift").show();
+  } else if (result > 6 && result <= 9 ) {
+    return "Your programming language suggestion is : Javascript";
+  } else if (result > 9 && result <= 12 ) {
+    return "Your programming language suggestion is : Python";
+  } else if (result > 12 && result <= 15 ) {
+    return "Your programming language suggestion is : Ruby";
+  } else {
+    alert("Please fill out all the question!");
+    // return "Please fill out all the question";
+  }
+}
 
 
 
-
-// user interface
+// user interface logic
 $(document).ready(function() {
   $(".userInput").submit(function(event) {
     event.preventDefault();
     $(".result").show();
-    const genderInp = parseInt($("#gender").val());
-    // console.log(genderInt);
-    const musicInp = parseInt($("#typemusic").val());
+    const edInp = parseInt($("#education").val());
+    const musicInp = parseInt($("#typeMusic").val());
     const colorInp = parseInt($("#color").val());
     const animalInt = parseInt($("#animal").val());
-    const totalResult = addResult(genderInp, musicInp, colorInp, animalInt);
-    $(".reult").text(codingLanguage(totalResult);)
+    const totalResult = addResult(edInp, musicInp, colorInp, animalInt);
+    $(".result").text(codingLanguage(totalResult));
   });
 });
 
 
-
-
-
-
-
-
-// result 0-4 : Swift
-// result 5-7 : Javascript
-// result 8-10 : Python 
-// result 11-13 : Ruby
-
+// $(".swift").show();
 
