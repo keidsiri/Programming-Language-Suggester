e828eb8b5e456602f09201f7f52ac69bba5b64c2
Kate Kiatsiri <keidsiri@gmail.com>
2021-10-22 14:41:29 -0700

    Adding more options on UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,29 +4,28 @@ function addResult(a,b,c,d) {
 }
 
 function codingLanguage(result) {
-  if (result > 3 && result <= 6 ) {
+  if (result > 3 && result <= 7 ) {
     $(".swift").show();
     $(".javascript").hide();
     $(".python").hide();
     $(".ruby").hide();
-  } else if (result > 6 && result <= 9 ) {
+  } else if (result > 7 && result <= 11 ) {
     $(".swift").hide();
     $(".javascript").show();
     $(".python").hide();
     $(".ruby").hide();
-  } else if (result > 9 && result <= 12 ) {
+  } else if (result > 11 && result <= 15 ) {
     $(".swift").hide();
     $(".javascript").hide();
     $(".python").show();
     $(".ruby").hide();
-  } else if (result > 12 && result <= 15 ) {
+  } else if (result > 15 && result <= 20 ) {
     $(".swift").hide();
     $(".javascript").hide();
     $(".python").hide();
     $(".ruby").show();
   } else {
     alert("Please fill out the question!");
-    // return "Please fill out all the question";
   }
 }
 
@@ -37,10 +36,10 @@ $(document).ready(function() {
   $(".userInput").submit(function(event) {
     event.preventDefault();
     const edInp = parseInt($("#education").val());
-    const musicInp = parseInt($("#typeMusic").val());
+    const backInt = parseInt($("#background").val());
     const colorInp = parseInt($("#color").val());
     const animalInt = parseInt($("#animal").val());
-    const totalResult = addResult(edInp, musicInp, colorInp, animalInt);
+    const totalResult = addResult(edInp, backInt, colorInp, animalInt);
     $(".result").text(codingLanguage(totalResult));
     $(".result").show();
   });
